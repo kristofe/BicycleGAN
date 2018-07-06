37dbaf9f7df911a15fc31dc130441af4b1e92df8
Kristofer Schalchter <kristofe72@gmail.com>
2018-07-06 16:23:10 -0400

    Got aligned dataset creator working

--- combine_A_and_B.py
+++ combine_A_and_B.py
@@ -1,7 +1,8 @@
 from pdb import set_trace as st
 import os
 import numpy as np
-import cv2
+#import cv2
+from PIL import Image
 import argparse
 
 parser = argparse.ArgumentParser('create image pairs')
@@ -15,35 +16,42 @@ args = parser.parse_args()
 for arg in vars(args):
     print('[%s] = ' % arg,  getattr(args, arg))
 
-splits = os.listdir(args.fold_A)
+args.use_AB = True
+img_fold_A = os.listdir(args.fold_A)
+img_fold_B = os.listdir(args.fold_B)
 
-for sp in splits:
-    img_fold_A = os.path.join(args.fold_A, sp)
-    img_fold_B = os.path.join(args.fold_B, sp)
-    img_list = os.listdir(img_fold_A)
-    if args.use_AB: 
-        img_list = [img_path for img_path in img_list if '_input.' in img_path]
+# this is for subdirectories i guess
+img_list = img_fold_A
+if args.use_AB:
+    img_list = [img_path for img_path in img_list if '_input.' in img_path]
 
-    num_imgs = min(args.num_imgs, len(img_list))
-    print('split = %s, use %d/%d images' % (sp, num_imgs, len(img_list)))
-    img_fold_AB = os.path.join(args.fold_AB, sp)
-    if not os.path.isdir(img_fold_AB):
-        os.makedirs(img_fold_AB)
-    print('split = %s, number of images = %d' % (sp, num_imgs))
-    for n in range(num_imgs):
-        name_A = img_list[n]
-        path_A = os.path.join(img_fold_A, name_A)
+num_imgs = min(args.num_imgs, len(img_list))
+print('use %d/%d images' % ( num_imgs, len(img_list)))
+img_fold_AB = args.fold_AB
+if not os.path.isdir(img_fold_AB):
+    os.makedirs(img_fold_AB)
+print('number of images = %d' % (num_imgs))
+for n in range(num_imgs):
+    name_A = img_list[n]
+    path_A = os.path.join(args.fold_A, name_A)
+    if args.use_AB:
+        name_B = name_A.replace('_input.', '_target_uint8.')
+    else:
+        name_B = name_A
+    path_B = os.path.join(args.fold_B, name_B)
+    if os.path.isfile(path_A) and os.path.isfile(path_B):
+        name_AB = name_A
         if args.use_AB:
-            name_B = name_A.replace('_input.', '_target_uint8.')
-        else:
-            name_B = name_A
-        path_B = os.path.join(img_fold_B, name_B)
-        if os.path.isfile(path_A) and os.path.isfile(path_B):
-            name_AB = name_A
-            if args.use_AB:
-                name_AB = name_AB.replace('_input.', '.') # remove _input
-            path_AB = os.path.join(img_fold_AB, name_AB)
-            im_A = cv2.imread(path_A, cv2.CV_LOAD_IMAGE_COLOR)
-            im_B = cv2.imread(path_B, cv2.CV_LOAD_IMAGE_COLOR)
-            im_AB = np.concatenate([im_A, im_B], 1)
-            cv2.imwrite(path_AB, im_AB)
+            name_AB = name_AB.replace('_input.', '.') # remove _input
+        path_AB = os.path.join(img_fold_AB, name_AB)
+        print path_A, path_B
+        im_A = np.array(Image.open(path_A))
+        im_B = np.array(Image.open(path_B))
+
+        im_B = np.expand_dims(im_B, axis=2)
+        im_B = np.repeat(im_B, 3, axis=2)
+
+        im_AB = np.concatenate([im_A, im_B], 1)
+        save_AB = Image.fromarray(im_AB)
+        save_AB.save(path_AB)
+
